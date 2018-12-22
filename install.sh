13f46043dab3e360d97af92ee9d64e6663c79a48
Tong Hui Kang <tonghuikang@outlook.com>
2018-12-22 20:21:58 +0800

    Update install.sh
    automatically run server.py

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
+# assumes Ubuntu 16.04, 10GB space, allow http and https
+
 export LC_ALL=C 
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get install git python3 python3-pip python3-tk -y
 pip3 install jupyter ipykernel 
+git clone https://github.com/tonghuikang/flask-example-minimal
+cd flask-example-minimal && python3 server.py
